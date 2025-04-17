230970d6316d3515f1a8e03fb9fd1841f26c96dc
DishaAggarwal31 <146239524+DishaAggarwal31@users.noreply.github.com>
2025-04-17 22:12:38 +0530

    Update Data_cleaning_before_quering_must_apply.sql
    Added the runners table cleaning steps

--- Case Study #2 - Pizza Runner/Data_cleaning_before_quering_must_apply.sql
+++ Case Study #2 - Pizza Runner/Data_cleaning_before_quering_must_apply.sql
@@ -1,3 +1,12 @@
+--Data cleaning and standarization
+
+--***Table customer_orders***
+SELECT * FROM customer_orders;
+
+--lets take a backup of the original table in case.
+CREATE TABLE customer_orders_original AS SELECT * FROM customer_orders;
+SELECT * FROM customer_orders_original;
+
 ---***Table customer_orders***
 
 --To replace the null or blanks values in exclusions column with null
@@ -27,3 +36,49 @@ SELECT * FROM runner_orders_original;
 SELECT * FROM runner_orders WHERE cancellation IS NULL OR TRIM(cancellation) == '' OR UPPER(cancellation) = 'NULL';
 UPDATE runner_orders
 SET cancellation = NULL WHERE cancellation IS NULL OR TRIM(cancellation) == '' OR UPPER(cancellation) = 'NULL';
+
+SELECT * FROM runner_orders;
+
+SELECT * FROM runner_orders WHERE distance IS NULL OR TRIM(distance) == '' OR UPPER(distance) = 'NULL';
+UPDATE runner_orders
+SET distance = 0 WHERE distance IS NULL OR TRIM(distance) == '' OR UPPER(distance) = 'NULL';
+
+SELECT * FROM runner_orders;
+
+SELECT * FROM runner_orders WHERE duration IS NULL OR TRIM(duration) == '' OR UPPER(duration) = 'NULL';
+UPDATE runner_orders
+SET duration = 0 WHERE duration IS NULL OR TRIM(duration) == '' OR UPPER(duration) = 'NULL';
+
+SELECT * FROM runner_orders;
+
+--Remove unnecessary characters from distance and duration
+SELECT distance, TRIM(SUBSTR(TRIM(distance),0, LENGTH(distance)-1)) FROM runner_orders WHERE lower(distance) LIKE '%km%';
+
+UPDATE runner_orders
+SET distance = TRIM(SUBSTR(TRIM(distance),0, LENGTH(distance)-1)) WHERE lower(distance) LIKE '%km%';
+
+--Can do if required ****
+--SELECT distance, distance || ' km' FROM runner_orders;
+--UPDATE runner_orders SET distance = distance || ' km';
+
+ALTER TABLE runner_orders RENAME distance TO distance_in_km;
+SELECT distance_in_km, CAST(distance_in_km AS REAL) FROM runner_orders;
+UPDATE runner_orders SET distance_in_km = CAST(distance_in_km AS REAL);
+
+SELECT * FROM runner_orders;
+
+SELECT duration, CAST(duration AS REAL)
+FROM runner_orders;
+
+UPDATE runner_orders
+SET duration = CAST(duration AS REAL);
+
+ALTER TABLE runner_orders RENAME duration TO duration_in_min;
+
+SELECT * FROM runner_orders;
+
+--***Other tables - As of now, no changes required***
+SELECT * FROM runners;
+SELECT * FROM pizza_names;
+SELECT * FROM pizza_recipes;
+SELECT * FROM pizza_toppings;
